92a86a1fd717e1affb5be6d4e0cc6a90f9e3b485
provence-jc <61579674+provence-jc@users.noreply.github.com>
2020-04-13 18:17:45 +0800

    按需加载和删除预加载

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,13 @@ module.exports = {
                 }
             }
         }
+    },
+    // publicPath:'/app',
+    // outputDir:'dist',
+    // indexPath:'index2.html',
+    // lintOnSave:false,
+    productionSourceMap: true,
+    chainWebpack: (config) => {
+        config.plugins.delete('prefetch');
     }
 }
